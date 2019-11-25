121c8bbe47d6155a2cd4013f24a7f7b6d49ea53e
Ubuntu <ubuntu@ip-172-31-20-220.ap-southeast-1.compute.internal>
2019-11-25 03:35:30 +0000

    Introduction to JSX

--- src/App.js
+++ src/App.js
@@ -2,24 +2,29 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+let styles = {
+  color:'red',
+  fontFamily: 'Verdana'
+}
+
 function App() {
+  
+  let message = <h1>We come in peace</h1>;
+  let blog = (<div><p>Line1</p><p>line 2</p></div>)
+  let x = 100;
+  if (x>10) {
+    styles.color='green'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div>
+    <p className='title'>Hello</p>
+    <p style={styles}>We bring cookies</p>
+    <p style={{ color:'orange', fontSize:'45px'}}>Orange</p>
+    {message}
+    { x }
+    {2+2}
+  </div>
+   
   );
 }
 
